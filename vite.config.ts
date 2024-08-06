9f98dcafa198a757744cb03625cd13e2f06c49b5
Grober Gonzales <GonzalesDLCGrober@gmail.com>
2024-08-06 18:26:47 -0500

    base GGonzalesD

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from 'node:path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'ggonzalesd',
+  base: 'GGonzalesD',
   resolve: {
     alias: [
       {
